c96b36b9553c4b83de759b865be17675e0475da2
Felipe Rondello <ferondellodev@gmail.com>
2024-08-06 20:49:06 -0300

    StringBuilder demo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace stringbuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
